d0d5129faf662662420ee42fd561ea220d9c9be2
Paula Ferreyra <paulaferreyra24@gmail.com>
2024-11-14 23:24:49 -0300

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Image from "next/image";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
+        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
+          <div className="flex items-center gap-5">
+            <Image
+              src="/assets/images/logo.png"
+              alt="logo"
+              width={80}
+              height={80}
+              className="h-auto"
+            />
+            <h1 className="text-3xl text-white ">CloudWise</h1>
+          </div>
+
+          <div className="space-y-5 text-white">
+            <h1 className="h1">Manage your files the best way</h1>
+            <p className="body-1">
+              This is a place where you can store all yout documents.
+            </p>
+          </div>
+          <div>
+            <Image
+              src="/assets/images/files.png"
+              alt="Files"
+              width={342}
+              height={342}
+              className="transition-all hover:rotate-2 hover:scale-105"
+            ></Image>
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center">
+        <div className="mb-16 lg:hidden">
+          <Image
+            src="/assets/icons/logo-full-brand.svg"
+            alt="logo"
+            width={100}
+            height={100}
+            className="h-auto w-[200px] lg:w-[250px]"
+          ></Image>
+        </div>
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default Layout;
